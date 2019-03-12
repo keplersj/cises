cac19500db3c178286e3d6af71f38844950bc1ef
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-03-11 18:19:35 -0600

    chore: working extension that can load RMP professor info
    Need to work on the UX of this. But the function works!

---
+++ src/college-scheduler.ts
@@ -0,0 +1,36 @@
+import queryString from "query-string";
+
+async function getProfessorFromSchool(teacherName: string, schoolName: string) {
+  const query = queryString.stringify({
+    bf: "pow(total_number_of_ratings_i,2.1)",
+    callback: "noCB",
+    defType: "edismax",
+    fq: `schoolname_t:"${schoolName}"`,
+    group: "off",
+    "group.field": "content_type_s",
+    "group.limit": "20",
+    "json.wrf": "noCB",
+    q: teacherName,
+    qf:
+      "teacherfirstname_t^2000 teacherlastname_t^2000 teacherfullname_t^2000 teacherfullname_autosuggest",
+    rows: ["20", "20"],
+    siteName: "rmp",
+    solrformat: "true",
+    sort: "score desc",
+    wt: "json"
+  });
+
+  const res = await fetch(
+    `https://search-production.ratemyprofessors.com/solr/rmp/select/?${query}`
+  );
+
+  function noCB(obj: any) {
+    return obj;
+  }
+
+  const { response } = eval(await res.text());
+
+  return response.docs[0];
+}
+
+// html body.static-header.modal-open div#base-modal.modal.fade.center-lg.in div.modal-dialog.modal-lg div.modal-content div.modal-body div.row div.col-md-6 ul.section-details li.persist
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    background: "./src/background.ts",
+    "college-scheduler": "./src/college-scheduler.ts"
+  },
+
+  output: {
+    // This copies each source entry into the extension dist folder named
+    // after its entry config key.
+    path: __dirname + "/dist",
+    filename: "[name].js"
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+
+  plugins: [
+    new CopyPlugin([{ from: "./src/manifest.json", to: "./manifest.json" }])
+  ]
+};
